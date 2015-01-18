b49e09fb72fa9e1d7aed7dda1a1e1989402f0d31
M.D. van Es <es@mdworld.nl>
2015-01-18 18:41:34 +0100

    added answer, review

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ var readline = require('readline'),
     port = '8041',
     project = 'export-Kinderbijslag',
     flow = 'Start',
-    version = '0.0-Wetwijziging';
+    version = '0.0-Wetwijziging',
+    state = null;
 
 // TODO wow such callback, much hell
 // TODO should be in a module
@@ -40,20 +41,33 @@ var getModel = function(sessionId) {
             data += chunk;
         });
         response.on('end', function() {
-            console.log('end: ', data, typeof data);
-            setTimeout(function() {
-                if(!data) {
-                    console.error('data is empty');
-                } else {
-                    try {
-                        var parsed = JSON.parse(data);
-                        console.log('parsed: ', parsed);
-                        //subscribe(parsed.sessionId);
-                    } catch(e) {
-                        console.error('could not parse');
-                    }
-                }
-            }, 1000);
+            // TODO stub
+            var stub = '{"elements":[{"key":"P960","name":"AanvragenKinderbijslag","properties":{},"children":["P960-C0","P960-C1","P960-C2","P960-C3"],"displayName":"Aanvragen kinderbijslag","contentStyle":"page","messages":[],"type":"page","styles":[]},{"key":"P960-C2-C0_T","name":null,"properties":{},"children":[],"displayName":null,"contentStyle":"table","type":"container","styles":[]},{"key":"P960-C1-F0","name":"Verzekerde.Voornaam","properties":{},"questionText":"Voornaam","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C2-C0-C0","name":"GeenKinderen","properties":{},"children":["P960-C2-C0-C0-TI0"],"displayName":null,"contentStyle":"container","type":"container","styles":[]},{"key":"P960-C1","name":"Verzekerde","properties":{},"children":["P960-C1-F0","P960-C1-F1","P960-C1-F2","P960-C1-F3","P960-C1-F4","P960-C1-F5","P960-C1-F6","P960-C1-F7"],"displayName":"Gegevens verzekerde","contentStyle":"container","type":"container","styles":[]},{"key":"P960-C2","name":"Aanvraag","properties":{},"children":["P960-C2-C0","P960-C2-B1"],"displayName":null,"contentStyle":"container","type":"container","styles":[]},{"key":"P960-C3","name":null,"properties":{},"children":["P960-C3-B0"],"displayName":null,"contentStyle":"container","type":"container","styles":[]},{"key":"P960-C0-TI0","name":"BerekenHoogteKinderbijslag","properties":{},"nodes":[{"text":"Met de kinderbijslag betaalt de overheid mee aan de kosten die horen bij de opvoeding van een kind. Woont of werkt u in Nederland en heeft u kinderen jonger dan 18 jaar, dan krijgt u waarschijnlijk kinderbijslag. Hoeveel kinderbijslag u krijgt is o.a. afhankelijk van de leeftijd van uw kind. Wilt u weten hoeveel kinderbijslag u krijgt? Maak dan een proefberekening.","nodeType":"text"}],"type":"textitem","styles":[]},{"key":"P960-C2-C0-C0-TI0","name":"GeenKinderen","properties":{},"nodes":[{"text":"Er zijn (nog) geen kinderen aanwezig.","nodeType":"text"}],"type":"textitem","styles":[]},{"key":"P960-C1-F3","name":"Verzekerde.Adres","properties":{},"questionText":"Adres","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C1-F4","name":"Verzekerde.Postcode","properties":{},"questionText":"Postcode","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":true,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C1-F1","name":"Verzekerde.Achternaam","properties":{},"questionText":"Achternaam","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C1-F2","name":"Verzekerde.Geslacht","properties":{},"questionText":"Wat is uw geslacht?","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":true,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[{"displayValue":"Man","value":"Man"},{"displayValue":"Vrouw","value":"Vrouw"}],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C1-F7","name":"Verzekerde.BankGiroRekeningNummer","properties":{},"questionText":"Bank/giro rekeningnummer","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":true,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C2-B1","name":"Nieuw","properties":{},"disabled":false,"refresh":false,"caption":"Kind toevoegen","type":"button","styles":[]},{"key":"P960-C2-C0","name":"Kinderen","properties":{},"children":["P960-C2-C0_T","P960-C2-C0-C0"],"displayName":"Kinderen","contentStyle":"instance_selector","type":"container","styles":[]},{"key":"P960-C3-B0","name":"Bereken","properties":{},"disabled":false,"refresh":false,"caption":"Bereken kinderbijslag","type":"button","styles":[]},{"key":"P960-C1-F5","name":"Verzekerde.Woonplaats","properties":{},"questionText":"Woonplaats","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C0","name":"Infobanner","properties":{},"children":["P960-C0-TI0"],"displayName":"Proefberekening kinderbijslag","contentStyle":"container","type":"container","styles":[]},{"key":"P960-C1-F6","name":"Verzekerde.Land","properties":{},"questionText":"Land","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":true,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[{"displayValue":"Nederland","value":"Nederland"},{"displayValue":"België","value":"Belgie"},{"displayValue":"Duitsland","value":"Duitsland"},{"displayValue":"Frankrijk","value":"Frankrijk"},{"displayValue":"Spanje","value":"Spanje"},{"displayValue":"Portugal","value":"Portugal"},{"displayValue":"Turkije","value":"Turkije"},{"displayValue":"Zwitserland","value":"Zwitserland"},{"displayValue":"Overig","value":"Overig"}],"messages":[],"values":[],"type":"field","styles":[]}],"language":{"patterns":{"datetime":"dd-mm-yyyy hh:mm:ss","date":"dd-mm-yyyy"},"languageCode":"nl-NL"}}';
+            try {
+                var parsed = JSON.parse(stub);
+                console.log('parsed: ', parsed);
+                state = new State(parsed);
+                console.log('stub loaded');
+                rl.prompt();
+                //subscribe(parsed.sessionId);
+            } catch(e) {
+                console.error('could not parse');
+            }
+
+            //console.log('end: ', data, typeof data);
+            //setTimeout(function() {
+            //    if(!data) {
+            //        console.error('data is empty');
+            //    } else {
+            //        try {
+            //            var parsed = JSON.parse(data);
+            //            console.log('parsed: ', parsed);
+            //            //subscribe(parsed.sessionId);
+            //        } catch(e) {
+            //            console.error('could not parse');
+            //        }
+            //    }
+            //}, 1000);
         });
     };
 
@@ -80,7 +94,7 @@ var subscribe = function(sessionId) {
             console.log('end: ', data, typeof data);
             setTimeout(function() {
                 getModel(sessionId)
-            }, 1000);
+            }, 100);
             //if(!data) {
             //    console.error('data is empty');
             //} else {
@@ -127,7 +141,7 @@ var start = function() {
                     console.log('sessionId: ', parsed.sessionId);
                     setTimeout(function() {
                         subscribe(parsed.sessionId)
-                    }, 1000);
+                    }, 100);
                 } catch(e) {
                     console.error('could not parse');
                 }
@@ -138,10 +152,83 @@ var start = function() {
     http.request(options, callback).end();
 };
 
+var State = function(modelJson) {
+    this.model = modelJson;
+
+    this.questions = this.prepareQuestions();
+};
+State.prototype.prepareQuestions = function() {
+    var questions = [];
+    //console.log(this.model.elements.length);
+    var elements = this.model.elements;
+    for(var i = 0; i < elements.length; i++) {
+        if(elements[i].questionText) {
+            questions.push(elements[i]);
+        }
+    }
+    return questions;
+};
+State.prototype.getQuestions = function() {
+    //var questions = [];
+    ////console.log(this.model.elements.length);
+    //var elements = this.model.elements;
+    //for(var i = 0; i < elements.length; i++) {
+    //    if(elements[i].questionText) {
+    //        questions.push(elements[i]);
+    //    }
+    //}
+    return this.questions;
+};
+State.prototype.isReady = function() {
+    return this.model;
+};
+
+var look = function() {
+    if(state.isReady) {
+        var questions = state.getQuestions();
+        //console.log(questions.length);
+        console.log('I have ' + questions.length + ' questions for you:');
+        for(var i = 0; i < questions.length; i++) {
+            var question = questions[i];
+            console.log(i + ')', question.questionText);
+        }
+    } else {
+        console.error('First run "start"!');
+    }
+};
+
+var answer = function(answerIndex) {
+    if(state.isReady()) {
+        //console.log(state.getQuestions()[answerIndex].questionText);
+        rl.question(state.getQuestions()[answerIndex].questionText + '? ', function(answer) {
+            console.log('> You answered ' + answer);
+            rl.prompt();
+        });
+    } else {
+        console.error('First run "start"!');
+    }
+};
+
+var review = function() {
+    if(state.isReady) {
+        var questions = state.getQuestions();
+        //console.log(questions.length);
+        console.log('I have ' + questions.length + ' questions for you:');
+        for(var i = 0; i < questions.length; i++) {
+            var question = questions[i];
+            console.log(i + ')', question.questionText + ' => ' + question.values[0]);
+        }
+    } else {
+        console.error('First run "start"!');
+    }
+};
+
 var help = function() {
     console.log('help for Blueriq CLI');
     console.log('start - initialize session');
-    console.log('ls (or show) - show all questions')
+    console.log('ls (or look) - show all questions');
+    console.log('answer n - answer question n');
+    console.log('answer all - answer all questions, starting with the first and automatically continuing until all are answered.')
 };
 
 // TODO fix so cursor is at end of line
@@ -149,27 +236,39 @@ rl.setPrompt('blueriq:~$ ');
 rl.prompt();
 
 rl.on('line', function(line) {
-    switch(line.trim()) {
-        case 'hello':
-            console.log('world!');
-            break;
-        case 'start':
-            start();
-            break;
-        case 'look':
-            break;
-        case 'next':
-            break;
-        case 'review':
-            break;
-        case 'help':
-            help();
-            break;
-        default:
-            console.log('Unknown command: `' + line.trim() + '`');
-            break;
+    if(line.trim().indexOf('answer') === 0) {
+        var param = line.trim().split(' ');
+        //console.log('answer ' + param[1]);
+        answer(param[1]);
+        //rl.prompt();
+    } else {
+        switch(line.trim()) {
+            case 'hello':
+                console.log('world!');
+                break;
+            case 'start':
+                start();
+                break;
+            case 'look':
+                look();
+                break;
+            case 'answer':
+                answer(0);
+                break;
+            case 'next': // next page
+                break;
+            case 'review':
+                review();
+                break;
+            case 'help':
+                help();
+                break;
+            default:
+                console.log('Unknown command: `' + line.trim() + '`');
+                break;
+        }
+        rl.prompt();
     }
-    rl.prompt();
 }).on('close', function() {
     // TODO how to trigger close?
     console.log('Have a great day!');
