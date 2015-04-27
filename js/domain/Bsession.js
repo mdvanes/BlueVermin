4add89aeaa9d86e8d00692926f4ad1a93acc1d05
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2015-04-27 14:15:07 +0200

    refactored to use request-promise

--- js/domain/Bsession.js
+++ js/domain/Bsession.js
@@ -1,10 +1,12 @@
 (function() {
     'use strict';
 
+    //var Q = require('q');
     //var config = require('../config'),
     //var http = require('follow-redirects').http;
     var State = require('./State'),
-        request = require('request');
+    //var request = require('request');
+        request = require('request-promise');
 
     // Blueriq Session initializer / Blueriq SessionController?
     var Bsession = function() {//state, rl) {
@@ -35,7 +37,9 @@
             //'pragma': 'no-cache',
             //'referer': 'http://lap-2077:8041/server/session/09b389e5-d76a-46a8-8bfd-7cb22f283722/mvc/index.html'
         };
-        request.get(
+
+        // TODO version before promises
+        /*request.get(
             {
                 url: 'http://lap-2077:8041/server/start?project=export-Kinderbijslag&flow=Start&version=0.0-Trunk&languageCode=nl-NL&ui=mvc&theme=cli&noTools=true',
                 jar: true,
@@ -78,7 +82,102 @@
                                 }
                             });
                     });
+            });*/
+
+
+        // TODO test npm package request-promise
+        console.log('request-promise start');
+        var fooSessionId; // TODO rename
+        request.get({
+            url: 'http://lap-2077:8041/server/start?project=export-Kinderbijslag&flow=Start&version=0.0-Trunk&languageCode=nl-NL&ui=mvc&theme=cli&noTools=true',
+            jar: true,
+            headers: blueriqInitHeaders
+        //}).then(console.dir).catch(console.error);
+        }).then(function(response) {
+            //console.log('stage 1:', response);//err, httpResponse, body);
+            if(response) {
+                var initJson = JSON.parse(response);
+                console.log('sessionId_' + initJson.sessionId, initJson);
+                fooSessionId = initJson.sessionId;
+            } else {
+                throw new Error('no body returned');
+            }
+            return request.post({
+                url: 'http://lap-2077:8041/server/session/' + fooSessionId + '/api/subscribe/', // TODO to contruct function
+                jar: true,
+                headers: blueriqInitHeaders
             });
+        }).then(function(){//response){
+            return request.post({
+                url: 'http://lap-2077:8041/server/session/' + fooSessionId + '/api/subscribe/' + fooSessionId, // TODO to construct function
+                jar: true,
+                headers: blueriqInitHeaders // TODO this is never used? only jar:true needed?
+            });
+        }).then(function(response){
+            console.log('get page foo:', response); // TODO log this if debug toggle is on
+            try {
+                // TODO stub
+                //var stub = '{"elements":[{"key":"P960","name":"AanvragenKinderbijslag","properties":{},"children":["P960-C0","P960-C1","P960-C2","P960-C3"],"displayName":"Aanvragen kinderbijslag","contentStyle":"page","messages":[],"type":"page","styles":[]},{"key":"P960-C2-C0_T","name":null,"properties":{},"children":[],"displayName":null,"contentStyle":"table","type":"container","styles":[]},{"key":"P960-C1-F0","name":"Verzekerde.Voornaam","properties":{},"questionText":"Voornaam","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C2-C0-C0","name":"GeenKinderen","properties":{},"children":["P960-C2-C0-C0-TI0"],"displayName":null,"contentStyle":"container","type":"container","styles":[]},{"key":"P960-C1","name":"Verzekerde","properties":{},"children":["P960-C1-F0","P960-C1-F1","P960-C1-F2","P960-C1-F3","P960-C1-F4","P960-C1-F5","P960-C1-F6","P960-C1-F7"],"displayName":"Gegevens verzekerde","contentStyle":"container","type":"container","styles":[]},{"key":"P960-C2","name":"Aanvraag","properties":{},"children":["P960-C2-C0","P960-C2-B1"],"displayName":null,"contentStyle":"container","type":"container","styles":[]},{"key":"P960-C3","name":null,"properties":{},"children":["P960-C3-B0"],"displayName":null,"contentStyle":"container","type":"container","styles":[]},{"key":"P960-C0-TI0","name":"BerekenHoogteKinderbijslag","properties":{},"nodes":[{"text":"Met de kinderbijslag betaalt de overheid mee aan de kosten die horen bij de opvoeding van een kind. Woont of werkt u in Nederland en heeft u kinderen jonger dan 18 jaar, dan krijgt u waarschijnlijk kinderbijslag. Hoeveel kinderbijslag u krijgt is o.a. afhankelijk van de leeftijd van uw kind. Wilt u weten hoeveel kinderbijslag u krijgt? Maak dan een proefberekening.","nodeType":"text"}],"type":"textitem","styles":[]},{"key":"P960-C2-C0-C0-TI0","name":"GeenKinderen","properties":{},"nodes":[{"text":"Er zijn (nog) geen kinderen aanwezig.","nodeType":"text"}],"type":"textitem","styles":[]},{"key":"P960-C1-F3","name":"Verzekerde.Adres","properties":{},"questionText":"Adres","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C1-F4","name":"Verzekerde.Postcode","properties":{},"questionText":"Postcode","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":true,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C1-F1","name":"Verzekerde.Achternaam","properties":{},"questionText":"Achternaam","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C1-F2","name":"Verzekerde.Geslacht","properties":{},"questionText":"Wat is uw geslacht?","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":true,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[{"displayValue":"Man","value":"Man"},{"displayValue":"Vrouw","value":"Vrouw"}],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C1-F7","name":"Verzekerde.BankGiroRekeningNummer","properties":{},"questionText":"Bank/giro rekeningnummer","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":true,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C2-B1","name":"Nieuw","properties":{},"disabled":false,"refresh":false,"caption":"Kind toevoegen","type":"button","styles":[]},{"key":"P960-C2-C0","name":"Kinderen","properties":{},"children":["P960-C2-C0_T","P960-C2-C0-C0"],"displayName":"Kinderen","contentStyle":"instance_selector","type":"container","styles":[]},{"key":"P960-C3-B0","name":"Bereken","properties":{},"disabled":false,"refresh":false,"caption":"Bereken kinderbijslag","type":"button","styles":[]},{"key":"P960-C1-F5","name":"Verzekerde.Woonplaats","properties":{},"questionText":"Woonplaats","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":false,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[],"messages":[],"values":[],"type":"field","styles":[]},{"key":"P960-C0","name":"Infobanner","properties":{},"children":["P960-C0-TI0"],"displayName":"Proefberekening kinderbijslag","contentStyle":"container","type":"container","styles":[]},{"key":"P960-C1-F6","name":"Verzekerde.Land","properties":{},"questionText":"Land","explainText":null,"dataType":"text","rejectedValue":null,"readonly":false,"required":true,"hasDomain":true,"multiValued":false,"refresh":false,"displayLength":-1,"domain":[{"displayValue":"Nederland","value":"Nederland"},{"displayValue":"België","value":"Belgie"},{"displayValue":"Duitsland","value":"Duitsland"},{"displayValue":"Frankrijk","value":"Frankrijk"},{"displayValue":"Spanje","value":"Spanje"},{"displayValue":"Portugal","value":"Portugal"},{"displayValue":"Turkije","value":"Turkije"},{"displayValue":"Zwitserland","value":"Zwitserland"},{"displayValue":"Overig","value":"Overig"}],"messages":[],"values":[],"type":"field","styles":[]}],"language":{"patterns":{"datetime":"dd-mm-yyyy hh:mm:ss","date":"dd-mm-yyyy"},"languageCode":"nl-NL"}}';
+                //var pageObj = JSON.parse(stub);
+                var pageObj = JSON.parse(response);
+                //console.log('parsed: ', parsed);
+                self.state = new State(pageObj);
+                //console.log('state', self.state, ' self ', self);
+                //console.log('stub loaded');
+                //this.state = state;
+                //this.rl.prompt();
+            } catch(e) {
+                console.error('could not parse', e);
+            }
+        })
+        .catch(console.error);
+
+        // TODO remove request (not request-promise) from package.json?
+
+
+        // TODO test promises, from https://github.com/bellbind/using-promise-q/blob/master/README.md
+        //var httpGet = function(opts) { // TODO rename requestGet
+        //    var deferred = Q.defer();
+        //    request.get(opts, deferred.resolve);
+        //    return deferred.promise;
+        //};
+        //var requestPost = function (opts) { // TODO rename requestPost
+        //    var deferred = Q.defer();
+        //    request.post(opts, deferred.resolve);
+        //    return deferred.promise;
+        //};
+        // chain-to-action-flatten.js
+        //console.log('promises start');
+        //httpGet({
+        //    url: 'http://lap-2077:8041/server/start?project=export-Kinderbijslag&flow=Start&version=0.0-Trunk&languageCode=nl-NL&ui=mvc&theme=cli&noTools=true',
+        //    jar: true,
+        //    headers: blueriqInitHeaders
+        //}).then(function (err,httpResponse,body) {
+        //    console.log('test', body, httpResponse);
+        //    if(body) {
+        //        var initJson = JSON.parse(body);
+        //        console.log(initJson);
+        //        console.log('promises: sessionId_' + initJson.sessionId, initJson, httpResponse, err);
+        //    } else {
+        //        throw new Error('body was undefined');
+        //    }
+        //
+        //    return body;
+        //    //return httpGet(url.parse(res.headers['location']));
+        //})
+        //.fail(function(error) {
+        //    console.error('error: ' + error, error);
+        //});
+        /*.then(function (res) {
+                return loadBody(res);
+        }).then(function (body) {
+            console.log(body);
+        });*/
+        //Make it more simple as:
+        // chain-to-action-simplify.js
+        //httpGet(url.parse("http://example.org")).then(function (res) {
+        //    return httpGet(url.parse(res.headers["location"]));
+        //}).then(loadBody).then(console.log);
     };
 
     //Bsession.prototype.getState = function() {
