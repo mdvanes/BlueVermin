09e3b06f2555ce34cb78f08dd656facfac988830
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2015-04-27 15:19:27 +0200

    added url constructors

--- js/config.js
+++ js/config.js
@@ -5,13 +5,13 @@
 (function() {
     'use strict';
 
-    var config = {
+    module.exports = {
         host: 'lap-2077',
         port: '8041',
         project: 'export-Kinderbijslag',
         flow: 'Start',
-        version: '0.0-Trunk' //'0.0-Wetwijziging',
+        version: '0.0-Trunk', //'0.0-Wetwijziging',
+        theme: 'cli'
     };
 
-    module.exports = config;
 })();
